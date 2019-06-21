2c54dc867a3100211f9c372a25696c3de9deec30
Michael Clayton <michael.clayton@unit9.com>
2019-06-21 12:58:53 +0100

    All paper equations working

--- Brian/code/examples/thalamusEquations/diffExample.py
+++ Brian/code/examples/thalamusEquations/diffExample.py
@@ -10,9 +10,18 @@ import math
 tau = 10 * ms
 equations = []
 
-# Membrane voltage dynamics
+# Set Membrane voltage dynamics
+Vrest = -65
 membraneVoltageEquation = '''
-    dVpre/dt = .01*(Vrest-Vpre) / tau : 1
+    dVpre/dt = .001*(Vrest-Vpre) / tau : 1
+'''
+sigma = .2
+randomInput = '''
+    dVpre/dt = sigma*sqrt(2/tau)*xi : 1
+'''
+sinusoidalInput = '''
+    dVmag/dt = .002*(0 - Vmag) / tau : 1
+    Vpre = -65 + (Vmag*sin(2*pi*(.4*Hz)*t)) : 1
 '''
 
 # -----------------------------
@@ -31,7 +40,6 @@ equations.append(equation1)
 # Equation 2
 alpha = 1000**-1 # * mM**-1 * second**-1
 beta = 50**-1 # * second**-1 # reverse rates of chemical reactions
-Vrest = -70
 equation2 = '''
     dr/dt = ( alpha * T * (1 - r) - beta * r ) / tau : 1
     diff = ( alpha * T * (1 - r) - beta * r ) : 1
@@ -92,49 +100,104 @@ equation5 = '''
 '''
 equations.append(equation5)
 
-# Combined equation for GABAb synapses
-eqs = membraneVoltageEquation
-eqsOfInterest = [0,2,3,4]
-for eq in eqsOfInterest:
-    eqs += equations[eq]
-G = NeuronGroup(1, eqs, method='euler')
-M = StateMonitor(G, [ 'Vpre', 'R', 'X', 'r'], record=True)
-run(2000*ms)
+# # Combined equation for GABAb synapses
+# eqs = membraneVoltageEquation + ''' : 1'''
+# eqsOfInterest = [0,2,3,4]
+# for eq in eqsOfInterest:
+#     eqs += equations[eq]
+# G = NeuronGroup(1, eqs, method='euler')
+# M = StateMonitor(G, [ 'Vpre', 'R', 'X', 'r'], record=True)
+# run(2000*ms)
 
-subplot(2,2,1)
-plot(M.t/ms, M.Vpre[0])
-ylabel('Vpre')
-subplot(2,2,2)
-plot(M.t/ms, M.R[0])
-ylabel('R')
-subplot(2,2,3)
-plot(M.t/ms, M.X[0])
-ylabel('X')
-subplot(2,2,4)
-plot(M.t/ms, M.r[0])
-ylabel('r')
+# subplot(2,2,1)
+# plot(M.t/ms, M.Vpre[0])
+# ylabel('Vpre')
+# subplot(2,2,2)
+# plot(M.t/ms, M.R[0])
+# ylabel('R')
+# subplot(2,2,3)
+# plot(M.t/ms, M.X[0])
+# ylabel('X')
+# subplot(2,2,4)
+# plot(M.t/ms, M.r[0])
+# ylabel('r')
+# # show()
+# savefig('equation3to5_results.png')
+
+
+# -----------------------------
+# Equation for post-synaptic current
+# -----------------------------
+
+# Equation 6 - post-synaptic current
+'''Returns current density A/m2'''
+Cuvw = 7.1 # 23.6
+gSynMax = 300 / 1000 #* uS/cm2 # maximum conductance
+ESyncRev = 0 #* mV # reverse potential
+equation6 = '''
+    Ipsp = Cuvw * gSynMax * r * (Vpsp - ESyncRev) : 1
+'''
+equations.append(equation6)
+#Cuvw * gSynMax * M.r[0][0] * (M.Vpsp[0][0] - ESyncRev)
+
+# Equation 7 - post-synaptic membrane potential
+km = 10 * ms #uF/cm2
+equation7 = '''
+    dVpsp/dt = - (Ipsp + Ileak) / km : 1
+'''
+equations.append(equation7)
+'''Note that this equation is different to the one listed in this paper.
+Here, Ipsp and Ileak are summed rather than subtracted. See the following
+webpage https://www.st-andrews.ac.uk/~wjh/hh_model_intro/ to see an explanation
+of why I think that summing them is correct. Suggesting to subtract them may have
+been a mistake in the original paper?'''
+
+# Equation 8 - leak current
+gLeak = 10 / 1000 #* uS/cm2
+Eleak = -55 #* mV
+equation8 = '''
+    Ileak = gLeak * (Vpsp - Eleak) : 1
+    # Ileak = gLeak * (Eleak - Vpsp) : 1
+'''
+equations.append(equation8)
+# gLeak/1000 * (M.Vpsp[0][0] - Eleak)
+
+# # Combined equation for GABAb synapses
+# # eqs = membraneVoltageEquation + ''' : 1'''
+# eqs = sinusoidalInput
+# eqsOfInterest = [0,1,5,6,7]
+# for eq in eqsOfInterest:
+#     eqs += equations[eq]
+# G = NeuronGroup(1, eqs, method='euler')
+# # G.Vpre = -20
+# G.Vpsp = -65
+# G.Vmag = 49
+# G.r = .001
+# M = StateMonitor(G, [ 'Vpre', 'Vpsp', 'r', 'Ipsp', 'Ileak'], record=True)
+# run(10000*ms)
+
+# print(M.Ipsp[0])
+# rows = 2; columns = 3
+# subplot(rows,columns,1)
+# plot(M.t/ms, M.Vpre[0])
+# ylabel('Vpre')
+# subplot(rows,columns,2)
+# plot(M.t/ms, M.Vpsp[0])
+# ylabel('Vpsp')
+# subplot(rows,columns,3)
+# plot(M.t/ms, M.Ipsp[0])
+# ylabel('Ipsp')
+# subplot(rows,columns,4)
+# plot(M.t/ms, M.Ileak[0])
+# ylabel('Ileak')
+# subplot(rows,columns,5)
+# plot(M.t/ms, M.r[0])
+# ylabel('r')
 # show()
-savefig('equation3to5_results.png')
+# # savefig('equation6to8_results.png')
 
 
 
-# # # # Equation 1 - neurotransmitter concentrations
 
-# # tau = 10 * ms
-# # omega = 3.8 * mV # steepness of the sigmoid function
-# # Tmax = 1 * mM
-# # Vthr = -32 * mV # Point where output reaches .5 mM
-# # alpha = 1000 * mM**-1 * second**-1
-# # beta = 50 * second**-1 # reverse rates of chemical reactions
-# # eqs = '''
-# # dVpre/dt = -100 * mV / tau : volt
-# # T = Tmax / (1 + exp(-((Vpre - Vthr)/omega))) : mM
-# # dr/dt = alpha * T * (1 - r) - beta * r / tau : mM
-# # '''
-# # G = NeuronGroup(1, eqs, method='euler')
-# # G.Vpre = -10 * mV
-# # M = StateMonitor(G, [ 'T', 'Vpre', 'r'], record=True)
-# # run(5*ms)
 
-# # r = .01
-# # alpha * T * (1 - r) - beta * r
+
