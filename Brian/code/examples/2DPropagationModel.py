bae37a8deaf0ab0bba24007f4ef725683f556841
Michael Clayton <michael.clayton@unit9.com>
2019-07-21 00:15:11 +0100

    Change connectivity to probabilistic, and add random synaptic noise

--- Brian/code/examples/2DPropagationModel.py
+++ Brian/code/examples/2DPropagationModel.py
@@ -4,9 +4,9 @@ import matplotlib.animation as animation
 import numpy as np
 
 # Define simulation parameters
-simulationLength = 250*ms
+simulationLength = 100*ms
 useVariableDelays = True
-groupLength = 7
+groupLength = 13
 def getCentralLocation(groupLength):
     firstRowLength = (groupLength-1)
     columnsToCenter = groupLength * (int(groupLength/2)-1)
@@ -29,12 +29,12 @@ def euclideanDistance(postSynapticLocation, preSynapticLocation):
 # Define function to get weight between two neurons
 locations = [[x,y] for x in range(groupLength) for y in range(groupLength)]
 @implementation('numpy', discard_units=True)
-@check_units(i=1, j=1, result=1)
-def getDistance(i,j):
+@check_units(i=1, j=1, sig=1, result=1)
+def getDistance(i, j, sig=3):
     preSynapticLocation = np.array(locations[int(i)])
     postSynapticLocation = np.array(locations[int(j)])
     distance = euclideanDistance(postSynapticLocation, preSynapticLocation)
-    return gaussian(distance, sig=2)
+    return gaussian(distance, sig)
 
 
 # -------------------------------------------
@@ -44,33 +44,29 @@ print('Creating network...')
 
 # Define neurons
 N = groupLength**2
-tau = 10.0*ms
+tau = 10*ms
 eqs = '''
-    du/dt = (-u + ISyn)/ tau : 1
+    du/dt = (-u + ISyn)/tau + (.1*xi*tau**-0.5) : 1
     dISyn/dt = -ISyn * ms**-1 : 1
 '''
-G = NeuronGroup(N, eqs, threshold='u>1', reset='u=0', method='exact')
+G = NeuronGroup(N, eqs, threshold='u>1', reset='u=0', method='euler')
 trace = StateMonitor(G, ['u', 'ISyn'], record=True)
 
 # Define synapses
-S = Synapses(G, G, on_pre='''ISyn += 3*getDistance(i,j)''', multisynaptic_index='synapse_number', method='euler')
-S.connect(condition='i!=j')
-# S = Synapses(G, G, on_pre='''ISyn += 2''', method='euler')
-# S.connect(condition='i!=j', p='getDistance(i,j)') # Use connection probability instead?
+S = Synapses(G, G, on_pre='''ISyn += 3''', method='euler')
+S.connect(condition='i!=j', p='getDistance(i,j)') # Use connection probability instead?
+# S = Synapses(G, G, on_pre='''ISyn += 3*getDistance(i,j)''', multisynaptic_index='synapse_number', method='euler')
+# S.connect(condition='i!=j')
 
 # Define synapse delays
 if useVariableDelays==True:
-    iterableLocations = [(i_row, i_col, j_row, j_col) for i_row in range(groupLength) \
-        for i_col in range(groupLength) for j_row in range(groupLength) \
-            for j_col in range(groupLength) if not(i_row==i_col and j_row==j_col)]
-    for index, location in enumerate(iterableLocations):
-        preSynapticLocation = np.array(location[0:1])
-        postSynapticLocation = np.array(location[2:])
-        distance = euclideanDistance(postSynapticLocation, preSynapticLocation)
-        S.delay[index] = 3*(1/gaussian(distance, sig=2))*ms
+    delayGaussianSigma = 3
+    for syn in range(len(S)):
+        currentI, currentJ = S.i[syn], S.j[syn]
+        S.delay[syn] = 1/getDistance(currentI, currentJ, delayGaussianSigma) * ms
 
 # Connect random input
-randomInput = PoissonGroup(50, np.arange(50)*Hz + 80*Hz)
+randomInput = PoissonGroup(50, np.arange(50)*Hz + 25*Hz)
 Sinput = Synapses(randomInput, G[sourceNeuron], on_pre = '''ISyn += .6''', method='euler')
 Sinput.connect()
 print('Running simulation...')
@@ -83,20 +79,22 @@ run(simulationLength, report='text')
 
 # Animation
 fig = figure(2, figsize=(4,4))
+fig.set_facecolor((.8,.8,.8))
+gca().set_facecolor((.8,.8,.8))
 
 # Initialise figure
 neuronPlots = []
 for neuron in range(trace.u.shape[0]):
     # if neuron==sourceNeuron: continue
     currentColor = str(trace.u[neuron][0])
-    neuronPlots.append(scatter(locations[neuron][0], locations[neuron][1], color=currentColor, s=500))
+    neuronPlots.append(scatter(locations[neuron][0], locations[neuron][1], color=currentColor, s=400))
 axis('off')
 
 # Loop figure
 def animate(t):
     for neuron in range(trace.u.shape[0]):
         # if neuron==sourceNeuron: continue
-        currentColor = str(trace.u[neuron][t])
+        currentColor = str(np.clip(trace.u[neuron][t], a_min=0, a_max=1))
         neuronPlots[neuron].set_color(currentColor)
     return neuronPlots
 myAnimation = animation.FuncAnimation(fig, animate, frames=trace.u.shape[1], interval=1, blit=True, repeat=False)
