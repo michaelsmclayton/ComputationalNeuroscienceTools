044b46527a175f607fbe41d7319687732fbe5639
Michael Clayton <michael.clayton@unit9.com>
2019-07-22 19:08:59 +0100

    Add matrix plotting and animation saving to mp4

--- Brian/code/examples/2DPropagationModel.py
+++ Brian/code/examples/2DPropagationModel.py
@@ -1,5 +1,6 @@
 from brian2 import *
 from matplotlib.pyplot import *
+matplotlib.use("Agg")
 import matplotlib.animation as animation
 import numpy as np
 
@@ -7,7 +8,7 @@ import numpy as np
 simulationLength = 500*ms
 neuronType = 'Kuramoto' # 'LIF'
 useVariableDelays = True
-groupLength = 13
+groupLength = 20
 N = groupLength**2
 def getCentralLocation(groupLength):
     firstRowLength = (groupLength-1)
@@ -37,6 +38,25 @@ def getDistance(i, j, sig=3):
     postSynapticLocation = np.array(locations[int(j)])
     distance = euclideanDistance(postSynapticLocation, preSynapticLocation)
     return gaussian(distance, sig)
+
+# Post-processing 1D to 2D data
+def getRowIndices(groupLength):
+    rowStarts = [(i*groupLength) for i in range(groupLength)]
+    rowEnds = [((i+1)*groupLength) for i in range(groupLength)]
+    return rowStarts, rowEnds
+def convertToMatrix(data, groupLength):
+    # Get row starts and ends
+    rowStarts, rowEnds = getRowIndices(groupLength)
+    # Initialise and then fill matrix
+    if len(data.shape)==1:
+        matrix = np.zeros(shape=[groupLength, groupLength])
+        for row in range(groupLength):
+            matrix[row,:] = data[range(rowStarts[row], rowEnds[row])]
+    elif len(data.shape)==2:
+        matrix = np.zeros(shape=[groupLength, groupLength, data.shape[-1]])
+        for row in range(groupLength):
+            matrix[row,:,:] = data[range(rowStarts[row], rowEnds[row]),]
+    return matrix
 
 
 # -------------------------------------------
@@ -79,6 +99,8 @@ if useVariableDelays==True:
     for syn in range(len(S)):
         currentI, currentJ = S.i[syn], S.j[syn]
         S.delay[syn] = 1/getDistance(currentI, currentJ, delayGaussianSigma) * ms
+        # 1/getDistance(currentI, currentJ, delayGaussianSigma) * ms
+        # euclideanDistance(currentI, currentJ) / 3.5*ms
 
 # # Connect random input
 # randomInput = PoissonGroup(50, np.arange(50)*Hz + 25*Hz)
@@ -107,28 +129,67 @@ fig = figure(2, figsize=(4,4))
 fig.set_facecolor((.8,.8,.8))
 gca().set_facecolor((.8,.8,.8))
 
+# Set up formatting for the movie files
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+
 # Get trace of interest
 if neuronType == 'LIF': 
     traceOfInterest = trace.u
 elif neuronType == 'Kuramoto': 
     traceOfInterest = (cos(trace.Theta)+1)/2
-
-# Initialise figure
-neuronPlots = []
-for neuron in range(traceOfInterest.shape[0]):
-    # if neuron==sourceNeuron: continue
-    currentColor = str(traceOfInterest[neuron][0])
-    neuronPlots.append(scatter(locations[neuron][0], locations[neuron][1], color=currentColor, s=500))
 axis('off')
 
-# Loop figure
-def animate(t):
-    for neuron in range(traceOfInterest.shape[0]):
-        # if neuron==sourceNeuron: continue
-        currentColor = str(np.clip(traceOfInterest[neuron][t], a_min=0, a_max=1))
-        neuronPlots[neuron].set_color(currentColor)
-    return neuronPlots
-myAnimation = animation.FuncAnimation(fig, animate, frames=traceOfInterest.shape[1], interval=1, blit=True, repeat=False)
-show()
+# Initialise figure
+def updatefig(t):
+    global thetaMatrix
+    im.set_array(np.transpose(thetaMatrix[:,:,t]))
+    if t > 10:
+        fig.set_facecolor((0,0,0))
+    return im,
+thetaMatrix = cos(convertToMatrix(trace.Theta, groupLength))
+im = imshow(thetaMatrix[:,:,0], animated=True)
+im.set_cmap('bone')
+ani = animation.FuncAnimation(fig, updatefig, frames=traceOfInterest.shape[1], interval=50, blit=True)
+ani.save('lines.mp4', writer=writer)
+# show()
 
 
+
+# --------------------------------------------------
+# Circles plot
+# --------------------------------------------------
+# # Initialise figure
+# neuronPlots = []
+# for neuron in range(traceOfInterest.shape[0]):
+#     # if neuron==sourceNeuron: continue
+#     currentColor = str(traceOfInterest[neuron][0])
+#     neuronPlots.append(scatter(locations[neuron][0], locations[neuron][1], color=currentColor, s=500))
+# axis('off')
+
+# # Loop figure
+# def animate(t):
+#     for neuron in range(traceOfInterest.shape[0]):
+#         # if neuron==sourceNeuron: continue
+#         currentColor = str(np.clip(traceOfInterest[neuron][t], a_min=0, a_max=1))
+#         neuronPlots[neuron].set_color(currentColor)
+#     return neuronPlots
+# myAnimation = animation.FuncAnimation(fig, animate, frames=traceOfInterest.shape[1], interval=1, blit=True, repeat=False)
+# show()
+
+# --------------------------------------------------
+# Experimental plot
+# --------------------------------------------------
+# def f(x, y):
+#     return np.sin(x) + np.cos(y)
+# x = np.linspace(0, 2 * np.pi, 120)
+# y = np.linspace(0, 2 * np.pi, 100).reshape(-1, 1)
+# im = imshow(f(x, y), animated=True)
+# def updatefig(*args):
+#     global x, y
+#     x += np.pi / 15.
+#     y += np.pi / 20.
+#     im.set_array(f(x, y))
+#     return im,
+# ani = animation.FuncAnimation(fig, updatefig, interval=50, blit=True)
+# show()
