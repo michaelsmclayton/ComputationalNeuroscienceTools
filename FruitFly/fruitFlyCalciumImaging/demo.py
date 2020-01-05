1327da623e8ee00a8f496a515989e3fb263d38b5
Michael Clayton <michael.clayton@unit9.com>
2020-01-04 22:12:46 -0600

    Code added to analyse large NII files, load from pre-saved data, and plot maximum intensity projection animation

--- FruitFly/fruitFlyCalciumImaging/demo.py
+++ FruitFly/fruitFlyCalciumImaging/demo.py
@@ -2,8 +2,10 @@ import numpy as np
 import nibabel as nib
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+# from scipy.signal import savgol_filter
+from nilearn import image
 from scipy.stats import zscore
-from scipy.io import loadmat
+from scipy.io import loadmat, savemat
 import gzip
 
 # Data and code from:
@@ -13,6 +15,18 @@ import gzip
 # "Fast near-whole brain imaging in adult Drosophila during responses to stimuli and behavior.
 # Sophie Aimon, Takeo Katsuki, Tongqiu Jia, Logan Grosenick, Michael Broxton, Karl Deisseroth,
 # Terrence J. Sejnowski, Ralph J Greenspan bioRxiv 033803; doi: https://doi.org/10.1101/033803"
+
+# Filenames
+imageNIIPath = "./data/GOL1_psfproj_dff_kf.nii" # AOL1_psfproj_dff_kf GOL6_psfproj_dff_kf
+imageStimInfo = "./data/GOL1FlashOdor.mat" # AOL1FlashOdor GOL6FlashOdor
+
+# Which data to load?
+loadFullNII = False
+loadSavedData = True
+saveData = False
+saveName = imageNIIPath[7:11]
+importDataInSegments = True # Use when NII file is too big
+
 
 # --------------------------------------------
 # Load data
@@ -27,24 +41,30 @@ def takeTimeExtract(startIndex, endIndex):
     return img_data[:,:,startIndex:endIndex,:] # At 200 there is a visual stimulus
 
 # # Load raw fluorescence image ----------------
-# rawData = loadNII(image_path='./AOL1b.nii')
-# sliceImage = np.mean(rawData[:,:,:,-500],axis=2)
-# np.save('sliceImage', sliceImage)
-# sliceImage = np.load('sliceImage.npy')
+# if loadSavedData==True:
+#     sliceImage = np.load('sliceImage.npy')
+# else:
+#     rawData = loadNII(image_path='./AOL1b.nii')
+#     sliceImage = np.mean(rawData[:,:,:,-500],axis=2)
+#     np.save('sliceImage', sliceImage)
 
-# # Load image data ----------------
-# img_data = loadNII(image_path='./AOL1_psfproj_dff_kf.nii')
-# cutImgData = takeTimeExtract(startIndex=700, endIndex=6700)
-# cutImgData = np.transpose(cutImgData, axes=(1,0,2,3))
-# np.save('cutImgData', cutImgData)
-cutImgData = np.load('cutImgData.npy')
+# Load image data ----------------
+if loadFullNII==True:
+    if loadSavedData==True:
+        cutImgData = np.load(saveName + '_cutImgData.npy')
+    else:
+        img_data = loadNII(image_path=imageNIIPath)
+        cutImgData = takeTimeExtract(startIndex=700, endIndex=6700)
+        cutImgData = np.transpose(cutImgData, axes=(1,0,2,3))
+        # if saveData==True:
+        #     np.save(saveName+'_cutImgData', cutImgData)
 
 
 # --------------------------------------------
 # Import stimulation information
 # --------------------------------------------
 print('Getting stimulation information...')
-matImport = loadmat("AOL1FlashOdor.mat")
+matImport = loadmat(imageStimInfo)
 def getStimulationTimes(odorType='visual'):
     odorIndex = 0 if odorType=='visual' else 1
     stimTimes = np.where(matImport['X'][odorIndex,:]==1)[0]
@@ -56,6 +76,7 @@ def getStimulationTimes(odorType='visual'):
 flashTimes = getStimulationTimes(odorType='visual')
 odorTimes = getStimulationTimes(odorType='odor')
 
+
 # --------------------------------------------
 # Get evoked brain activity
 # --------------------------------------------
@@ -63,11 +84,21 @@ frequency = 200 # Hz
 baselineLength = 1 * frequency # seconds
 evokedLength = 2 * frequency
 def getMeanEvokedActivity(stimulationTimes):
+
     evokedData = []
-    for time in stimulationTimes:
+    for i, time in enumerate(stimulationTimes):
+        print('Getting evoked activity %s/%s...' % (i+1,len(stimulationTimes)))
         startTime = time - baselineLength
         endTime = time + evokedLength
-        evokedData.append(img_data[:,:,startTime:endTime,:])
+        # If the data should be imported in segments (i.e. for big images)
+        if importDataInSegments==True:
+            currentEvoked = []
+            for t in range(startTime, endTime):
+                singleTimePointImage = image.index_img(imageNIIPath, t)
+                currentEvoked.append(singleTimePointImage.get_fdata())
+            evokedData.append(np.transpose(currentEvoked, (1,2,0,3)))
+        else: # else... (i.e. for smaller images)
+            evokedData.append(img_data[:,:,startTime:endTime,:])
     evokedData = np.array(evokedData)
 
     # # Baseline subtraction
@@ -83,10 +114,14 @@ def getMeanEvokedActivity(stimulationTimes):
     evokedAverage = np.transpose(evokedAverage, axes=(1,0,2,3))
     return evokedAverage
 
-# visualEvokedAverage = getMeanEvokedActivity(stimulationTimes=flashTimes)
-# odorEvokedAverage = getMeanEvokedActivity(stimulationTimes=odorTimes)
-# np.save('visualEvokedAverage', visualEvokedAverage)
-visualEvokedAverage = np.load('baselineCorrectedVisualEvokedAverage.npy')
+if loadSavedData==True:
+    visualEvokedAverage = np.load('full' + saveName + 'visualEvokedAverage.npy')
+else:
+    visualEvokedAverage = getMeanEvokedActivity(stimulationTimes=flashTimes)
+    # odorEvokedAverage = getMeanEvokedActivity(stimulationTimes=odorTimes)
+    if saveData==True:
+        np.save(saveName +  'visualEvokedAverage', visualEvokedAverage)
+        print('Visual evoked activity saved!')
 times = np.linspace(-baselineLength, evokedLength, num=visualEvokedAverage.shape[2])/frequency
 
 # --------------------------------------------
@@ -110,7 +145,7 @@ def makeAnimation(data, sliceIndex=None, showTimes=False):
             return [im, text]
         else: return im
     fig = plt.figure()# figsize=(10,10))
-    im = plt.imshow(retrievalFunction(data, 0, sliceIndex)) #, cmap="gray")
+    im = plt.imshow(retrievalFunction(data, 0, sliceIndex))#, cmap="gray")
     if showTimes == True: text = plt.text(5,5, s="%.2f" % times[0], color='white')
     frameNumber = np.max(data.shape)
     anim = animation.FuncAnimation(fig, animate_func, frames=frameNumber, interval = 1.0) #1000 / fps)
@@ -118,6 +153,10 @@ def makeAnimation(data, sliceIndex=None, showTimes=False):
     plt.show()
 
 # Dispay evoked average
+# makeAnimation(visualEvokedAverage, sliceIndex=5, showTimes=True)
+makeAnimation(np.max(visualEvokedAverage, axis=3), sliceIndex=None, showTimes=True) # Maximal projection
 # makeAnimation(np.mean(visualEvokedAverage, axis=3), sliceIndex=None, showTimes=True)
-makeAnimation(np.mean(cutImgData, axis=3), sliceIndex=None)
+if loadFullNII==True:
+    makeAnimation(np.mean(cutImgData, axis=3), sliceIndex=None)
 # makeAnimation(visualEvokedAverage, sliceIndex=1)
+
