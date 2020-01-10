b3fe16a0315601630363df1bd8aa2fa53435e0df
Michael Clayton <michael.clayton@unit9.com>
2020-01-10 17:24:01 -0500

    Code added to evolve synaptic travel towards an output neuron

--- CircularEvolution/circularEvolution.py
+++ CircularEvolution/circularEvolution.py
@@ -3,26 +3,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from numpy.random import normal, uniform
+from scipy.spatial import distance
 
 # Parameters
-numberOfNeurons = 30
+numberOfNeurons = 4
+populationSize = 10
+generations = 15
 spaceRange = [-2.5,2.5]
 pi = np.pi
 
 # -----------------------------------------
-# Core functions
+# Setup functions
 # -----------------------------------------
 
 # Make neurons
-def makeNeuron():
+def makeNeuron(x, y):
     return {
-        'xPos': uniform(spaceRange[0],spaceRange[1]),
-        'yPos': uniform(spaceRange[0],spaceRange[1]),
-        'nSynapses': int(uniform(50,100)),
+        'xPos': x, # uniform(spaceRange[0],spaceRange[1]),
+        'yPos': y, # uniform(spaceRange[0],spaceRange[1]),
+        'nSynapses': 50, # int(uniform(50,100)),
         'angleHomogeneity': uniform(1,60),
         'meanAngle': uniform(0, pi),
         'angleDistScale': 1, # divides the distribution
-        'distanceSTD': normal(1.0,.2), 
+        'distanceSTD': normal(1.0,.3), 
     }
 
 # Get connections
@@ -48,44 +51,139 @@ def getConnections(neuron):
 
     return synapses
 
+# -----------------------------------------
+# Fitness and reproduction functions
+# -----------------------------------------
+
+# Get connections that touch
+def getFitness(population):
+    # threshold = .25
+    # validConnections = []
+    minDistances = []
+    for sourceNeuron in range(numberOfNeurons):
+        synapses = getConnections(population[sourceNeuron])
+        for synapse in synapses:
+            synapseXLine = np.linspace(synapse[0][0], synapse[0][1])
+            synapseYLine = np.linspace(synapse[1][0], synapse[1][1])
+            for targetNeuron in range(numberOfNeurons,numberOfNeurons+1): #range(numberOfNeurons):
+                if sourceNeuron==targetNeuron: continue
+                targetPosition = [population[targetNeuron]['xPos'], population[targetNeuron]['yPos']]
+                distances = []
+                for i in range(len(synapseXLine)):
+                    currentLinePosition = [synapseXLine[i], synapseYLine[i]]
+                    currentDistance = distance.euclidean(np.array(currentLinePosition), np.array(targetPosition))
+                    distances.append(currentDistance)
+                    # if currentDistance < threshold:
+                    #     validConnections.append([sourceNeuron, targetNeuron, synapse])
+                minDistances.append(np.min(distances))
+    # fitness = len(validConnections)
+    fitness = 100 - np.mean(minDistances)
+    return fitness #, validConnections
+
+# Make new generation
+def makeNewGeneration(populations, fitnessesInOrder):
+
+    # Select parents
+    def getParent(fitnessesInOrder):
+        randomNumber = uniform(0,1)
+        if randomNumber<.5:
+            parent = fitnessesInOrder[-1]
+        elif randomNumber<.75:
+            parent = fitnessesInOrder[-2]
+        elif randomNumber<.875:
+            parent = fitnessesInOrder[-3]
+        else:
+            parent = fitnessesInOrder[-3]
+        return parent
+
+    # Make new populations
+    newPopulations = []
+    for p in range(populationSize):
+        
+        # Get parents
+        parentA = []; parentB = []
+        while parentA == parentB:
+            parentA = getParent(fitnessesInOrder)
+            parentB = getParent(fitnessesInOrder)
+        parents = [populations[parentA], populations[parentB]]
+
+        # Breed parents
+        parents = [populations[fitnessesInOrder[-1]], populations[fitnessesInOrder[-2]]]
+        newSpecimen = [{} for i in range(numberOfNeurons)]
+        values = list(parents[0][0].keys())
+        for i in range(numberOfNeurons):
+            for value in values:
+                if uniform(0,1)>.5:
+                    parent = 0
+                else:
+                    parent = 1
+                newSpecimen[i][value] = parents[parent][i][value]
+                # Random mutation
+                if uniform(0,1)<.025:
+                    newNeuron = makeNeuron(parents[parent][i]['xPos'], parents[parent][i]['yPos'])
+                    newSpecimen[i][value] = newNeuron[value]
+
+        newSpecimen.append(makeNeuron(x=(numberOfNeurons-1)/2,y=1))
+        newPopulations.append(newSpecimen)
+    
+    return newPopulations
 
 # -----------------------------------------
 # Main script
 # -----------------------------------------
 
-# Make neurons
-neurons = [makeNeuron() for i in range(numberOfNeurons)]
+# Setup initial population
+populations = []
+for p in range(populationSize):
+    neurons = [makeNeuron(x,y=0) for x in range(numberOfNeurons)]
+    neurons.append(makeNeuron(x=(numberOfNeurons-1)/2,y=1))
+    populations.append(neurons)
 
-# # Get connections that touch (...work in progress...)
-# print('Getting valid connections...')
-# threshold = .15
-# validConnections = []
-# for sourceNeuron in range(numberOfNeurons):
-#     synapses = getConnections(neurons[sourceNeuron])
-#     for synapse in synapses:
-#         synapseXLine = np.linspace(synapse[0][0], synapse[0][1])
-#         synapseYLine = np.linspace(synapse[1][0], synapse[1][1])
-#         for targetNeuron in range(numberOfNeurons):
-#             if sourceNeuron==targetNeuron: continue
-#             targetPosition = [neurons[targetNeuron]['xPos'], neurons[targetNeuron]['yPos']]
-#             for i in range(len(synapseXLine)):
-#                 currentLinePosition = [synapseXLine[0], synapseYLine[0]]
-#                 distance = np.linalg.norm(np.array(currentLinePosition) - np.array(targetPosition))
-#                 if distance < threshold:
-#                     validConnections.append([sourceNeuron, targetNeuron, synapse])
+# Loop over generations
+for g in range(generations):
+    print('---Generation %s' % (g))
 
-# Plot neurons
-for neuron in neurons:
-    plt.scatter(neuron['xPos'], neuron['yPos'], c='black')
-    synapses = getConnections(neuron)
-    for synapse in synapses:
-        plt.plot([synapse[0][0], synapse[0][1]], [synapse[1][0], synapse[1][1]], alpha=0.2)
-plt.show()
-    
-# # Plot valid connections
-# for connection in validConnections:
-#     synapseInfo = connection[2]
-#     plt.plot([synapseInfo[0][0], synapseInfo[0][1]], [synapseInfo[1][0], synapseInfo[1][1]])
+    # Get fitnesses
+    fitnesses = []
+    for p in range(populationSize):
+        currentFitness = getFitness(populations[p])
+        print('Population %s fitness = %s' % (p, currentFitness))
+        fitnesses.append(currentFitness)
+    fitnessesInOrder = np.argsort(fitnesses)
+    fittestNetwork = populations[fitnessesInOrder[-1]]
+    print('Mean fitness = %s' % (np.mean(fitnesses)))
+
+    # Get new populations
+    populations = makeNewGeneration(populations, fitnessesInOrder)
+
+
+# Plot final fittest network
+def plotFittestNetwork(fittestNetwork):
+    # Plot neurons
+    for neuron in fittestNetwork:
+        plt.scatter(neuron['xPos'], neuron['yPos'], c='black')
+        synapses = getConnections(neuron)
+        for synapse in synapses:
+            plt.plot([synapse[0][0], synapse[0][1]], [synapse[1][0], synapse[1][1]], alpha=0.2)
+    plt.show()
+
+# # Plot final fittest network
+# def plotFittestNetwork(fittestNetwork, validConnections):
+#     # Plot neurons
+#     for neuron in fittestNetwork:
+#         plt.scatter(neuron['xPos'], neuron['yPos'], c='black')
+#         synapses = getConnections(neuron)
+#         for synapse in synapses:
+#             plt.plot([synapse[0][0], synapse[0][1]], [synapse[1][0], synapse[1][1]], alpha=0.2)
+#     # Plot valid connections
+#     for connection in validConnections:
+#         synapseInfo = connection[2]
+#         plt.plot([synapseInfo[0][0], synapseInfo[0][1]], [synapseInfo[1][0], synapseInfo[1][1]], color='black')
+#     plt.show()
+
+plotFittestNetwork(fittestNetwork)
+
+
 
 # Legacy code ----
 # # Plot connections    
