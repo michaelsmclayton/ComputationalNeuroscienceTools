b6a8d5ba9909eef78a799fcd2e0a097bf35cf23b
Michael Clayton <michael.clayton@unit9.com>
2020-04-16 17:07:33 +0200

    Greater population number, and alpha desynchronisation in absence of alpha input

--- Brian/code/examples/alphaThetaNetwork/alphaThetaNetwork.py
+++ Brian/code/examples/alphaThetaNetwork/alphaThetaNetwork.py
@@ -1,35 +1,61 @@
 from brian2 import *
 
+# -------------------------------
+# Interneurons
+# -------------------------------
+
 # Define functions
 gaussian = lambda amp,mu,sig : '''%s*exp(-((tC-%s)**2)/%s)''' % (amp,mu,sig)
-sawtooth = lambda A, T : '''%s*((t/ms/%s)-floor(t/ms/%s))''' % (A,T,T)
+sawtooth = lambda A : '''%s*((((t/ms)-tS)/T)-floor(((t/ms)-tS)/T))''' % (A)
+thetaNeuron = lambda sig : '''dTheta/dt = ( (1-cos(Theta)) + (I*(1 + cos(Theta)))) * ms**-1 : 1'''# + (%s*xi)*ms**-.5: 1''' % (sig)
 
 # Define function parameters
-A, T = 100, 100 # period of the sawtooth oscillations
+A = 100 # period of the sawtooth oscillations
 mu = 50; sig = 10 # moment and width of secondary, bursting activity
+gaussianHeight = 5
 
-# Define model
-eqs = '''
-    dTheta/dt = ( (1-cos(Theta)) + (I*(1 + cos(Theta)))) * ms**-1 + (.05*xi)*ms**-.5: 1
-    I = (1*(I_exp+I_gaus))-.1 : 1
-    I_exp = exp(-tC/2) : 1 # exponential
-    I_gaus = '''+gaussian(5,mu,sig)+''' : 1 # Gaussian
-    tC = '''+sawtooth(A,T)+''': 1 # cycle time
+# Define interneuron model
+numberOfNeurons = 100
+eqs = thetaNeuron(sig=.1) + '''
+    I = (I_exp+I_gaus)-.1 : 1
+    I_exp = exp(-tC/2) : 1 # Exponential (i.e. initial spike)
+    I_gaus = '''+gaussian(gaussianHeight,mu,sig)+''' : 1 # Gaussian (i.e. both spike burst)
+    tC = '''+sawtooth(A)+''': 1 # cycle time
+    tS : 1 # input spike time
+    T : 1
 '''
-G = NeuronGroup(1, eqs, method='euler')
-trace = StateMonitor(G, ['Theta','I', 'tC'], record=True)
+interneurons = NeuronGroup(N=numberOfNeurons, model=eqs, threshold="sin(Theta)>.99", method='euler')
+interneurons.T = 100+(np.random.randn(numberOfNeurons)*5)
+interneurons.tS = -np.abs(np.random.randn(numberOfNeurons))*50
+trace_interNeuron = StateMonitor(interneurons, ['Theta','I', 'tC', 'tS'], record=True)
+
+# -------------------------------
+# Bursting neurons
+# -------------------------------
+eqs = thetaNeuron(sig=0) + '''
+    I : 1
+'''
+bursting = NeuronGroup(N=1, model=eqs, threshold="sin(Theta)>.99", method='euler')
+trace_bursting = StateMonitor(bursting, ['Theta'], record=True)
+
+# -------------------------------
+# Synapses
+# -------------------------------
+S = Synapses(bursting, interneurons, on_pre='tS_post = t/ms') # reset sawtooth on spike
+S.connect()
 
 # Run and plot results
-run(200*ms)
-subplot(3,1,1)
-plot(np.sin(trace.Theta[0]))
-subplot(3,1,2)
-plot(trace.I[0])
-subplot(3,1,3)
-plot(trace.tC[0])
+bursting.I = -.1
+run(1000*ms)
+bursting.I = .001
+run(1000*ms)
+bursting.I = -.1
+run(2000*ms)
+fig, ax = plt.subplots(2,1,sharex=True)
+#ax[0].plot(trace_interNeuron.t/ms, np.sin(trace_interNeuron.Theta[0]))
+ax[0].plot(trace_interNeuron.t/ms, np.mean(np.sin(trace_interNeuron.Theta),axis=0))
+ax[1].plot(trace_bursting.t/ms, np.sin(trace_bursting.Theta[0]))
 show()
-
-
 
 
 
