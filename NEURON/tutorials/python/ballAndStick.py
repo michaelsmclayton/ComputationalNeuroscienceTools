615a1b4a8c47d92cd24677097ef4cc7cb8b59560
Michael Clayton <michael.clayton@unit9.com>
2020-05-08 18:33:18 +0200

    Add code to compile and load .mod files

--- NEURON/tutorials/python/ballAndStick.py
+++ NEURON/tutorials/python/ballAndStick.py
@@ -1,3 +1,4 @@
+import os
 from neuron import h
 from neuron.units import ms, mV, um
 import matplotlib.pylab as plt
@@ -9,14 +10,19 @@ pp = pprint.PrettyPrinter(depth=6).pprint
 # (e.g. running a simulation for a given period)
 h.load_file('stdrun.hoc')
 
+# Compile .mod files
+if not(os.path.isdir("./x86_64")):
+    os.system("nrnivmodl ./mod_files/")
+    h.nrn_load_dll('./x86_64/.libs/libnrnmech.so') # Load compiled .mod files
+
 class BallAndStick:
     '''A ball-and-stick cell by definition consists of two parts: the soma (ball) and a dendrite (stick).
     We could define two Sections at the top level (as in the previous tutorial ('scriptingBasics.py), but that
     wouldn't give us an easy way to create multiple cells. Instead, let's define a BallAndStick neuron class'''
-    def __init__(self, gid):
-        self._gid = gid
+    def __init__(self):
         self.soma = h.Section(name='soma', cell=self)
         self.dend = h.Section(name='dend', cell=self)
+        self.soma.insert('ca')
     '''Any variables that describe properties of the cell must get stored as attributes of self. This is why we
     write self.soma instead of soma. Temporary variables, on the other hand, need not be prefixed with self and
     will simply stop existing when the initialization function ends'''
@@ -31,3 +37,5 @@ my_cell = BallAndStick()
 
 # Print current cell topology
 h.topology()
+
+pp(my_cell.soma.psection())
