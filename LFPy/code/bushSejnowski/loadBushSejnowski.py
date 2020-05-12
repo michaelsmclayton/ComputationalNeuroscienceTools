4c38f9dd730cd480bfe0afef6b487b030fad1e6c
Michael Clayton <michael.clayton@unit9.com>
2020-05-12 14:29:47 +0200

    Tidy definition of dummy cell

--- LFPy/code/bushSejnowski/loadBushSejnowski.py
+++ LFPy/code/bushSejnowski/loadBushSejnowski.py
@@ -6,20 +6,20 @@ import LFPy
 import matplotlib.pyplot as plt
 import pprint; pp = pprint.PrettyPrinter(depth=6).pprint
 from matplotlib.animation import FuncAnimation
-from neuron import h
+from neuron import h #, gui
 
 # Simulation parameters
 global dt; dt = .1
-simulationLength = 1000
+simulationLength = 105
 timepoints = int(simulationLength/dt)+1
 
 # Define template directory, and load compiled .mod files (from NEURON file)
 templateDirectory = '../../../NEURON/code/hoc&mod/bushSejnowski/'
 h.nrn_load_dll(templateDirectory + '/x86_64/.libs/libnrnmech.so') # Load compiled .mod files
 
-# -----------------------------------------
-# Define neural network (creating cells from .hoc templates)
-# -----------------------------------------
+# ------------------------------------------------------------------------------------------------
+#                       Define neural network (creating cells from .hoc templates)
+# ------------------------------------------------------------------------------------------------
 
 # --------------------------------------
 # Get cells (LFPy.NetworkCell)
@@ -55,14 +55,12 @@ def addPopulation(network, cellParams, N, name):
         Cell=LFPy.NetworkCell,
         cell_args=cellParams,
         pop_args=dict(radius=1.,loc=0.,scale=0),
-        rotation_args=dict(x=np.pi/2, y=0),
-    )
+        rotation_args=dict(x=np.pi/2, y=0))
     network.create_population(name=name, POP_SIZE=N, **populationParameters)
 
 # Add populations to netowkr
 addPopulation(network, L2params, 1, 'L2pop')
 addPopulation(network, L5params, 1, 'L5pop')
-
 
 # Rotate all cells
 network.populations['L2pop'].cells[0].set_rotation(**{'x': -.2*np.pi, 'y': 0})
@@ -78,7 +76,7 @@ network.populations['L5pop'].cells[0].set_pos(x=00,y=0)
 def makeStimulus(cell):
     return LFPy.StimIntElectrode(
     cell=cell, idx=0, pptype='IClamp',
-    amp=5,dur=100., delay=5.,
+    amp=1,dur=100., delay=5.,
     record_current=True)
 makeStimulus(network.populations['L2pop'].cells[0])
 makeStimulus(network.populations['L5pop'].cells[0])
@@ -93,36 +91,24 @@ grid_electrode = LFPy.RecExtElectrode(**{
     'y' : Y.flatten(),
     'z' : Z.flatten()})
 
-# -----------------------------------------
-# Simulate, and plot results
-# -----------------------------------------
+# ------------------------------------------------------------------------------------------------
+#                                   Simulate, and plot results
+# ------------------------------------------------------------------------------------------------
+
+# Simulate
 network.simulate(rec_vmem=True, rec_imem=True)
 
 # Make dummy cell
-totnsegs = 0; imem = np.empty((0,timepoints))
-xstart, ystart, zstart = [np.empty(0) for i in range(3)]
-xmid, ymid, zmid = [np.empty(0) for i in range(3)]
-xend, yend, zend = [np.empty(0) for i in range(3)]
-diam, area = [np.empty(0) for i in range(2)]
+dummyCellParams = {}; dataTypes = ['xstart', 'ystart', 'zstart', 'xmid', 'ymid', 'zmid', 'xend', 'yend', 'zend', 'diam', 'area']
+dummyCellParams['totnsegs'] = 0; dummyCellParams['imem'] = np.empty((0,timepoints))
+for dataType in dataTypes: dummyCellParams[dataType] = np.empty(0)
 for pop in network.populations.keys():
     for cell in network.populations[pop].cells:
-        totnsegs += cell.totnsegs
-        imem = np.vstack((imem, cell.imem))
-        # for dataType in ['xstart', 'ystart', 'zstart', 'xmid', 'ymid', 'zmid', 'xend', 'yend', 'zend', 'diam', 'area']:
-        #     exec('np.hstack((%s,cell.%s)))' % (dataType, dataType))
-        xstart = np.hstack((xstart,cell.xstart))
-        ystart = np.hstack((ystart,cell.ystart))
-        zstart = np.hstack((zstart,cell.zstart))
-        xmid = np.hstack((xmid,cell.xmid))
-        ymid = np.hstack((ymid,cell.ymid))
-        zmid = np.hstack((zmid,cell.zmid))
-        xend = np.hstack((xend,cell.xend))
-        yend = np.hstack((yend,cell.yend))
-        zend = np.hstack((zend,cell.zend))
-        diam = np.hstack((diam,cell.diam))
-        area = np.hstack((area,cell.area))
-fullNetwork = LFPy.network.DummyCell(totnsegs=totnsegs, imem=imem, xstart=xstart, ystart=ystart, \
-    zstart=zstart, xmid=xmid, ymid=ymid, zmid=zmid, xend=xend, yend=yend, zend=zend, diam=diam, area= area)
+        dummyCellParams['totnsegs'] += cell.totnsegs
+        dummyCellParams['imem'] = np.vstack((dummyCellParams['imem'], cell.imem))
+        for dataType in dataTypes:
+            dummyCellParams[dataType] = np.hstack((dummyCellParams[dataType], getattr(cell,dataType)))
+fullNetwork = LFPy.network.DummyCell(**dummyCellParams)
 fullNetwork.verbose = False
 
 # Get LFP (reshaping into matrix: x,y,time)
@@ -141,22 +127,23 @@ fig, axs = plt.subplots(ncols=1, nrows=5)
 fig.set_figheight(8); fig.set_figwidth(3)
 gs = axs[0].get_gridspec()
 ax0 = fig.add_subplot(gs[1:])
-lfpPlot = ax0.imshow(np.rot90(LFP[:,:,0]), extent=np.r_[gridLims['x'],gridLims['y']], vmin=np.min(LFP), vmax=np.max(LFP), cmap='gist_gray')
+lfpPlot = ax0.imshow(np.rot90(LFP[:,:,0]), extent=np.r_[gridLims['x'],gridLims['y']], vmin=np.min(LFP), vmax=np.max(LFP))#, cmap='gist_gray')
 showNeuron(network.populations['L2pop'].cells[0],ax0)
 showNeuron(network.populations['L5pop'].cells[0],ax0)
-# ax1 = fig.add_subplot(gs[0])
-# ax1.plot(L5Cell.vmem.T, color='k', alpha=.1)
-# line, = ax1.plot([0,0],[np.min(L5Cell.vmem), np.max(L5Cell.vmem)], color='k')
+ax1 = fig.add_subplot(gs[0])
+ax1.plot(network.populations['L5pop'].cells[0].vmem.T, color='r', alpha=.1)
+ax1.plot(network.populations['L2pop'].cells[0].vmem.T, color='b', alpha=.1)
+line, = ax1.plot([0,0],[-80,50], color='k')
 for ax in axs: ax.axis('off')
-# ax1.axis('off')
-# ax0.axis('off'); 
+ax1.axis('off')
+ax0.axis('off'); 
 
 # Define animation function
 def updatefig(t):
     # print(L5Cell.tvec[t])
     lfpPlot.set_data(np.rot90(LFP[:,:,int(t)]))
-    # line.set_xdata([t,t])
-    return lfpPlot,#line
+    line.set_xdata([t,t])
+    return lfpPlot,line
 
 # Animate
 ani = FuncAnimation(fig, updatefig, frames=range(LFP.shape[2]), interval=2)
