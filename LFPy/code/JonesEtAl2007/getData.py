68d48cbd1bb13db25bcacc6241894ab7e58bb5a4
Michael Clayton <michael.clayton@unit9.com>
2020-05-13 11:53:05 +0200

    Delete .zip file after unzipping

--- LFPy/code/JonesEtAl2007/getData.py
+++ LFPy/code/JonesEtAl2007/getData.py
@@ -5,17 +5,19 @@ import zipfile
 
 # Get the model files (https://senselab.med.yale.edu/ModelDB/ShowModel?model=113732#tabs-1)
 modelLink = 'https://senselab.med.yale.edu/modeldb/eavBinDown?o=113732&a=23&mime=application/zip'
-
 u = urlopen(modelLink, context=ssl._create_unverified_context())
+
 # Save data to .zip
-localFile = open('SS-cortex.zip', 'wb')
+zipName = 'SS-cortex'
+localFile = open('%s.zip' % (zipName), 'wb')
 localFile.write(u.read())
 localFile.close()
 
 # Unzip file
-myzip = zipfile.ZipFile('SS-cortex.zip', 'r')
+myzip = zipfile.ZipFile('%s.zip' % (zipName), 'r')
 myzip.extractall('.')
 myzip.close()
+os.system('rm %s.zip' % (zipName)) # delete .zip file
 
 # Compile .hoc files
-os.system('cd SS-cortex; nrnivmodl')
+os.system('cd %s; nrnivmodl' % (zipName))
