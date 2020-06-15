51bad4e5ff4d1d087a6b025255529b3046079315
Michael Clayton <michael.clayton@unit9.com>
2020-06-15 21:06:49 +0200

    Add notes on plotting and distances from midline

--- natverse/tutorial/01_pyramidalNeuron.R
+++ natverse/tutorial/01_pyramidalNeuron.R
@@ -94,7 +94,7 @@ print(boutonsStrahler/branchPointStrahler)
 
 
 # ----------------------------------------------
-# Perform Sholl analysis
+# Perform Sholl analysis (dendritic intersections vs. distance from soma)
 # ----------------------------------------------
 "Sholl analysis is a method of quantitative analysis commonly used in neuronal studies to characterize
 the morphological characteristics of an imaged neuron, first used to describe the differences in the
@@ -107,15 +107,169 @@ and overall branching patterns of neurons."
 sholla = sholl_analysis(neuron)
 
 # Plot the result
-X11() # new ggplot figure
+if (rstudioRunning()==FALSE){X11()} # new ggplot figure
 ggplot(sholla, aes(x=radii, y=intersections, color="darkgrey")) + xlab('Distance from soma') +
   ylab('Dendritic intersections') + geom_line()+theme_minimal()
 
 # We could also plot the distance to boutons on this graph
-X11()
+if (rstudioRunning()==FALSE){X11()}
 root = neuron$d[rootpoints(neuron),c("X","Y","Z")]
 near2 = nabor::knn(data = root, query = xyzmatrix(boutons),k = 1)$nn.dist
 bdist = data.frame(intersections = 0, dist = near2)
 ggplot(data = bdist, aes(x=dist, color="red", fill = "red")) + 
   geom_density()+
   theme_minimal()
+
+
+# ----------------------------------------------
+# Work out which layers each bouton is in
+# ----------------------------------------------
+
+# Load layer information
+surface = read.csv("data/layer_lines_csv/surface_MAX_27_06_19_s4_c7_Pom.csv")[,c("X","Y")]
+surface$lboundary = "l1_surface"
+l23 = read.csv("data/layer_lines_csv/L23_L1_MAX_27_06_19_s4_c7_Pom.csv")[,c("X","Y")]
+l23$lboundary = "l23_l1"
+l4 = read.csv("data/layer_lines_csv/L4_L23_MAX_27_06_19_s4_c7_Pom.csv")[,c("X","Y")]
+l4$lboundary = "l4_23"
+l5 = read.csv("data/layer_lines_csv/L5_L4_MAX_27_06_19_s4_c7_Pom.csv")[,c("X","Y")]
+l5$lboundary = "l5_l4"
+l6 = read.csv("data/layer_lines_csv/L6_L5_MAX_27_06_19_s4_c7_Pom.csv")[,c("X","Y")]
+l6$lboundary = "l6_l5"
+wm = read.csv("data/layer_lines_csv/wm_L6_MAX_27_06_19_s4_c7_Pom.csv")[,c("X","Y")]
+wm$lboundary = "wm_l6"
+
+# Combine layer information together
+layers = rbind(surface,l23,l4,l5,l6,wm)
+layers$lower = gsub("_.*","",layers$lboundary) # Get lower layer
+layers$upper = gsub(".*_","",layers$lboundary) # Get upper layer
+
+# Convert values to microns (these coordinates are 2D, and in pixels)
+layers[,c("X","Y")] = layers[,c("X","Y")]/2.4089
+
+# Add a Z dimension (mean z-axis value)
+layers$Z = colMeans(xyzmatrix(neuron))["Z"]
+
+# Define function to assign layers
+assignLayers <- function(data){
+  "To assign a given layer, we need to find the closest point, and see if it is above or below the layer line"
+  near = nabor::knn(query = xyzmatrix(data), data = xyzmatrix(layers), k = 1)$nn.idx
+  data$nearest.lboundary = layers[near,"lboundary"]
+  data$nearest.lboundary.x = layers[near,"X"]
+  data$layer = ifelse(data$nearest.lboundary.x>data$X, 
+                      gsub("_.*","",data$nearest.lboundary),
+                      gsub(".*_","",data$nearest.lboundary))
+  return(data)
+}
+
+# Assign the branchpoint and boutons to layers
+boutons = assignLayers(boutons)
+neuron$d = assignLayers(neuron$d)
+
+# Calculate bouton density per layer
+node.layers = table(neuron$d$layer)
+boutons.layers = table(boutons$layer)[names(node.layers)]
+bouton.density = boutons.layers / node.layers
+
+# Plot neuron with respect to column divisions (and with boutons colored for each layer)
+clear3d()
+plot3d(as.neuronlist(neuron), col = "black", lwd = 3)
+spheres3d(boutons[,c("X","Y","Z")], col = layer.cols[boutons$layer], radius = 10)
+points3d(xyzmatrix(layers), col = layer.cols[layers$lower])
+
+
+# ----------------------------------------------
+# Combine data and plot results
+# ----------------------------------------------
+
+# Make data frame for plotting
+df = rbind(node.layers, boutons.layers,bouton.density)
+df = melt(df) # row for each data point
+colnames(df) = c("type", "layer", "value")
+
+# Nodes per layer
+ggplot(subset(df, grepl("node.layers",type)), aes(x=type, y=value, fill=layer)) + 
+  geom_bar(stat="identity") + scale_fill_manual(values = layer.cols) + 
+  ggtitle("Nodes per layer") + theme_minimal() + theme(plot.title = element_text(hjust = 0.5))
+
+# Boutons per layer
+ggplot(subset(df, grepl("boutons.layers",type)), aes(x=type, y=value, fill=layer)) +
+  geom_bar(stat="identity") + scale_fill_manual(values = layer.cols) +
+  ggtitle("Boutons per layer") + theme_minimal() + theme(plot.title = element_text(hjust = 0.5))
+
+# Bouton density per layer (bar chart)
+ggplot(subset(df, grepl("bouton.density",type)), aes(x=type, y=value, fill=layer)) +
+  geom_bar(stat="identity") + scale_fill_manual(values = layer.cols) +
+  ggtitle("Bouton density per layer") + theme_minimal() + theme(plot.title = element_text(hjust = 0.5))
+
+
+# ----------------------------------------------
+# Analyse Euclidean distance from neuron midline
+# ----------------------------------------------
+
+# Load midline data
+axis = read.csv("data/Axis_line/Axis_line_27_06_19_s4_c7.csv")[,c("X","Y")]
+axis[,c("X","Y")] = axis[,c("X","Y")]/2.4089 # Convert values to microns
+axis$Z = colMeans(xyzmatrix(neuron))["Z"] # Add z-axis
+
+# Get Euclidean bouton distances to the midline
+bouton.points = boutons[,c("X","Y","Slice")] # let's use the original bouton locations for this
+boutons$dist.to.midline = unlist(nabor::knn(query = bouton.points, data = axis, k = 1)$nn.dists)
+
+# Plot Boutons Euclidean Distance to midline
+ggplot(boutons, aes(x=dist.to.midline, color = layer, fill = layer)) + geom_histogram(binwidth=50) +
+  scale_fill_manual(values = layer.cols) + scale_color_manual(values = layer.cols) +
+  ggtitle("Boutons Euclidean Distance to midline") + theme_minimal() + theme(plot.title = element_text(hjust = 0.5))
+
+
+# ----------------------------------------------
+# Analyse Geodesic distance from neuron midline
+# ----------------------------------------------
+"To calculate geodesic distance, we are just going to count the number of points that separate boutons.
+Let's work this out per layer"
+
+# Create a directed graph
+graph = as.ngraph(neuron) # We can now use this representation with functions from the package iGraph.
+
+# Get a single distance value per layer
+boutons$interbouton.distance  = NA
+interbouton = data.frame()
+for(l in unique(boutons$layer)){
+  # Just boutons on one layers
+  bouton.nodes = unique(subset(boutons, layer == l)$treenode)
+  # So we want to take the mean distance for the bouton behind, and in front, of each bouton of interest
+  ## the distance to the next bouton, away from the root.
+  dists1 = distances(graph, v = bouton.nodes, to = bouton.nodes, mode = c("out"), weights = NULL)
+  diag(dists1) = NA
+  dist.out = apply(dists1, 1, function(x) min(x, na.rm = TRUE)) # node that cannot be reached have an infinite value
+  dist.out = dist.out[!is.infinite(dist.out)]
+  ## And towards it
+  dists2 = distances(graph, v = bouton.nodes, to = bouton.nodes, mode = c("in"), weights = NULL)
+  diag(dists2) = NA
+  dist.in = apply(dists2, 1, function(x) min(x, na.rm = TRUE)) # node that cannot be reached have an infinite value
+  dist.in = dist.in[!is.infinite(dist.in)]
+  interbouton = rbind(interbouton, data.frame(
+    layer = l,
+    mean = mean( c(dist.out,dist.in) ) / stepsize,
+    sd = sd( c(dist.out,dist.in) ) / stepsize,
+    number = length(bouton.nodes)
+  ))
+  ## Calculate on a single neuron basis
+  dist.both = intersect(names(dist.in), names(dist.out))
+  dists = c(dist.in[dist.both] + dist.out[dist.both])/2
+  missing1 = names(dist.out)[!names(dist.out)%in%dist.both]
+  missing2 = names(dist.in)[!names(dist.in)%in%dist.both]
+  dists[missing1] = dist.out[missing1]
+  dists[missing2] = dist.in[missing2]
+  dists = dists / stepsize
+  match.to.bouton = match(names(dists), boutons$treenode)
+  boutons[match.to.bouton,]$interbouton.distance = dists
+}
+
+# Plot Boutons Geodesic Distance to midline (jitter plot)
+ggplot(boutons, aes(x=layer, y=interbouton.distance, color = layer)) + 
+  geom_jitter(position=position_jitter(0.2)) + 
+  stat_summary(fun.data="mean_sdl", mult=1, geom="crossbar", width=0.5, col = "grey70") + 
+  stat_summary(fun.data=mean_sdl, mult=1, geom="pointrange", color="red")+
+  scale_color_manual(values = layer.cols) +
+  ggtitle("Boutons Geodesic Distance to midline") + theme_minimal() + theme(plot.title = element_text(hjust = 0.5))
