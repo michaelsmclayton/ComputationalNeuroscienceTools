c90889ce3555cd14d44b5acec579c15f478da662
Michael Clayton <michael.clayton@unit9.com>
2020-06-25 16:41:49 +0200

    Add code for visualising neurons, and add documentation

---
+++ Janelia/StringerEtAl2019/README.md
@@ -0,0 +1,18 @@
+# Neuroscience Data Challenge (Dataset 3)
+
+These scripts have been tested using a 2018 MacBook Pro (OS X 10.13.4)
+
+## Download data
+ - Before running scripts, download the required data using <a href="https://github.com/michaelsmclayton/ComputationalNeuroscienceTools/blob/master/Janelia/StringerEtAl2019/downloadData.py">downloadData.py</a> (requires wget)
+
+## Visualising the imaged neurons
+<p align=center>
+    <img width="75%" src="./figures/neuronVisualisation.gif"><br>
+    <a href="https://github.com/michaelsmclayton/ComputationalNeuroscienceTools/blob/master/Janelia/StringerEtAl2019/visualiseNeurons.py">visualiseNeurons.py</a>
+</p>
+
+## Assess neural correlations across image categories
+<p align=center>
+    <img width="85%" src="./figures/correlationsAcrossCategories.png"><br>
+    <a href="https://github.com/michaelsmclayton/ComputationalNeuroscienceTools/blob/master/Janelia/StringerEtAl2019/correlationsAcrossCategories.m">correlationsAcrossCategories.m</a>
+</p>
---
+++ Janelia/StringerEtAl2019/visualiseNeurons.py
@@ -0,0 +1,40 @@
+import numpy as np
+import scipy.io as sio
+from scipy.stats import gaussian_kde, zscore
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+# Import neuron data data
+baseDirectory = './data/'
+mt = sio.loadmat('%snatimg2800_M170714_MP032_2017-09-14.mat' % baseDirectory)
+
+# Get cell information
+med = mt['med']  # cell centers (X Y Z)
+numberOfNeurons = med.shape[0]
+
+# Normalise data (for color information)
+def normalise(med):
+    norm = med + np.min(med)
+    norm = norm / np.max(norm)
+    return norm
+normMed = normalise(med)
+
+# Add alpha channel
+alpha = .5
+normMed = np.hstack((normMed, alpha*np.ones(shape=[len(normMed),1])))
+
+# Show neurons
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+dots = ax.scatter(med[:,0], med[:,1], med[:,2], s=2, color=normMed)
+ax.set_xticks([]); ax.set_yticks([]); ax.set_zticks([])
+ax.set_facecolor((0,0,0))
+plt.axis('off')
+
+# Rotate the axes and update
+for angle in range(0,720,1):
+    print(angle)
+    ax.view_init(30, angle)
+    plt.draw()
+    plt.pause(.0001)
