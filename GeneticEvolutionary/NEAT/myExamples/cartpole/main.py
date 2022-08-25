3af3404098e77e881c1f56f3a3f8241195ed6a02
Michael Clayton <michaelsmclayton@googlemail.com>
2022-08-25 17:45:02 +0100

    Add initial code with NEAT (2022)

---
+++ GeneticEvolutionary/NEAT/myExamples/cartpole/main.py
@@ -0,0 +1,57 @@
+# --------------------
+# Define game
+# --------------------
+# requires pip3 install gym==0.21.0
+import gym
+env = gym.make("CartPole-v1")
+
+# Observation and action space 
+obs_space = env.observation_space
+action_space = env.action_space
+print("The observation space: {}".format(obs_space))
+print("The action space: {}".format(action_space))
+
+# --------------------
+# Define NEAT optimiser
+# --------------------
+import neat
+import numpy as np
+
+# Load configuration.
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, 'config-feedforward')
+
+# Create the population, which is the top-level object for a NEAT run.
+p = neat.Population(config)
+# Add a stdout reporter to show progress in the terminal.
+p.add_reporter(neat.StdOutReporter(False))
+
+# Evaluation genome
+def evaluate(env, net, timepoints, render=False):
+    obs = env.reset()
+    rewards = np.zeros(timepoints)
+    for t in range(timepoints):
+        actions = net.activate(obs)
+        # actions = np.random.choice([0,1])
+        actions = int(np.round(actions))
+        obs, reward, done, info = env.step(actions)
+        rewards[t] = reward
+        if render==True:
+            env.render()
+        if np.abs(obs[0])>4.8:
+            return t
+    return t*np.sum(rewards)
+
+def eval_genomes(genomes, config):
+    global env
+    for genome_id, genome in genomes:
+        net = neat.nn.RecurrentNetwork.create(genome, config)
+        genome.fitness = evaluate(env, net, 300)
+
+
+# Run until a solution is found.
+winner = p.run(eval_genomes, 40)
+
+# Run the winning genome.
+best_genome = p.best_genome
+net = neat.nn.RecurrentNetwork.create(best_genome, config)
+r = evaluate(env, net, 1000, render=True)
